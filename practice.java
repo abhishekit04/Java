68c2a6437e88983eaf188cd94322db5c2ee6b461
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2022-11-14 00:36:40 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,14 @@
+public class practice {
+    public static void main (String[] args) {
+
+        System.out.println("sum of these numbers: ");
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
+    }
+
+
+}
